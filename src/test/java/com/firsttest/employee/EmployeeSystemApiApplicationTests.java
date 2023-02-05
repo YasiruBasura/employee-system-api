a7ed62d4f6f17a58b10b7a0c16bcdd5077f43ca3
Yasi <113851370+YasiruBasura@users.noreply.github.com>
2023-02-05 17:36:24 +0530

    initailize

---
+++ src/test/java/com/firsttest/employee/EmployeeSystemApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.firsttest.employee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeSystemApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
