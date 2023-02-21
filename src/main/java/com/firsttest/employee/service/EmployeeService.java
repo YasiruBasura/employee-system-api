8e806209de40ccfafafef2b0e5d13cda551fc0bb
Yasi <113851370+YasiruBasura@users.noreply.github.com>
2023-02-21 18:42:31 +0530

    push failure

---
+++ src/main/java/com/firsttest/employee/service/EmployeeService.java
@@ -0,0 +1,9 @@
+package com.firsttest.employee.service;
+
+import com.firsttest.employee.model.Employee;
+
+public interface EmployeeService {
+
+
+    Employee createEmployee(Employee employee);
+}
